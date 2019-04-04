f028c69fea5b65eccb39cd6ee41aff04c1c1deeb
chidera <dexiouz@gmail.com>
2019-04-04 18:47:05 +0100

    Used apollo-client and apollo-provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import BookList from '../src/components/BookList';
 import './App.css';
+
+// apollo client set up
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
 
 class App extends Component {
   render() {
     return (
+      <ApolloProvider client={client}>
       <div className="App">
         <h1>Chidera Reading Lists</h1>
         <BookList />
       </div>
+      </ApolloProvider>
     );
   }
 }
