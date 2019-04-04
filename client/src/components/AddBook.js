2f357674d0f7982b8d6eab950365eb0845603896
chidera <dexiouz@gmail.com>
2019-04-04 20:05:22 +0100

    Set up form and updated state for adding new book

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -4,6 +4,11 @@ import { graphql } from 'react-apollo';
 
 
 class AddBook extends Component {
+  state = {
+    name: '',
+    genre: '',
+    authorId: ''
+  }
   displayAuthors() {
     const data = this.props.data;
     return data.loading ? (<option disabled>Loading books...</option>) :
@@ -11,22 +16,26 @@ class AddBook extends Component {
         data.authors.map(author => <option key={author.id} value={author.id}>{author.name}</option>)
       )
   }
+  submitForm = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+  }
   render() {
     return (
-      <form id="add-book">
+      <form id="add-book" onSubmit={this.submitForm}>
         <div className="field">
           <label> Book Name:</label>
-          <input type="text" />
+          <input type="text" onChange={(e) => this.setState({ name: e.target.value })} />
         </div>
 
         <div className="field">
           <label> Genre:</label>
-          <input type="text" />
+          <input type="text" onChange={(e) => this.setState({ genre: e.target.value })} />
         </div>
 
         <div className="field">
           <label>Author:</label>
-          <select>
+          <select onChange={(e) => this.setState({ authorId: e.target.value })}>
             <option>Select author</option>
             {this.displayAuthors()}
           </select>
