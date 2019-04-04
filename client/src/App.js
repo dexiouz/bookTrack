15c87f704ec7b5a1aad58e8fc5948322ad2b4a91
chidera <dexiouz@gmail.com>
2019-04-04 19:42:03 +0100

    imporrted addbook component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import BookList from '../src/components/BookList';
+import AddBook from '../src/components/AddBook';
 import './App.css';
 
 // apollo client set up
@@ -13,10 +14,11 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-      <div className="App">
-        <h1>Chidera Reading Lists</h1>
-        <BookList />
-      </div>
+        <div className="App">
+          <h1>Chidera Reading Lists</h1>
+          <BookList />
+          <AddBook />
+        </div>
       </ApolloProvider>
     );
   }
